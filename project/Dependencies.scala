ccd2d9d26f5eb0ed464bab93035c88f41a0ffca5
Junichi Kato <j5ik2o@gmail.com>
2024-07-06 10:18:15 +0900

    update scala version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,8 @@
 object Dependencies {
 
   object Versions {
-    val scala212Version = "2.12.10"
-    val scala213Version = "2.13.12"
+    val scala212Version = "2.12.19"
+    val scala213Version = "2.13.14"
     val scala3Version   = "3.0.0"
 
     val scalaCollectionCompatVersion = "2.4.4"
